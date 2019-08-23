8f4e97984774890033f93d5aaa86bb9e257beedc
Zach Pfeifer <mr.zman1@yahoo.com>
2019-08-23 13:05:52 -0600

    Model Started

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,4 +1,18 @@
 export default class List {
     //TODO You will need to create a constructor 
     //and the methods needed to create the view template for this model
+    constructor(data) {
+        console.log('Model Checking-in')
+        this.title = data.title
+        this.toDo = data.toDo
+    }
+    getTemplate(index) {
+        let template = `
+            <div class="col-4">
+            <h1>${this.title}</h1>
+            <h3>${this.toDo}</h3>
+            <ul>
+            `
+        return template
+    }
 }
